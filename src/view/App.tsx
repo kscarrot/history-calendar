65230fff0af8276f2b27577b95558dfd3ddb376b
kscarrot <kelstrait@icloud.com>
2025-02-19 13:36:38 +0800

    feat: 🎸 view ganzhi page

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,19 +1,35 @@
-import { useState } from 'react'
+import { 干支排列表 } from '@/constant/ganzhi'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const chunk = (arr: string[], size: number) => {
+    const result = []
+    for (let i = 0; i < arr.length; i += size) {
+      result.push(arr.slice(i, i + size))
+    }
+    return result
+  }
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          {chunk(干支排列表, 10).map((单行) => (
+            <tr>
+              {单行.map((单列) => (
+                <td>{单列}</td>
+              ))}
+            </tr>
+          ))}{' '}
+        </p>
+        <p>
+          {chunk(干支排列表, 12).map((单行) => (
+            <tr>
+              {单行.map((单列) => (
+                <td>{单列}</td>
+              ))}
+            </tr>
+          ))}{' '}
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: 'src/view',
+  root: 'src',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
